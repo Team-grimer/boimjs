c26f4927a4f1a954ef6650ba59ca4d42d9de1534
eunoia2040 <eunoia2040@gmail.com>
2022-05-31 15:41:58 +0900

    chore: Initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "error",
+    indent: ["warn", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+    "no-var": "error",
+    "no-unused-vars": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", ["parent", "sibling"]],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
