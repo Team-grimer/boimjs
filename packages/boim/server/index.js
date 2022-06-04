c96b2f110dd23e23195049f5ad78e88136e4fc19
seodongsu <xl445566@naver.com>
2022-06-05 02:19:09 +0900

    docs: Move boimjs -> boim

--- packages/boim/server/index.js
+++ packages/boim/server/index.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
     }
 
     htmlFile = data.replace('<div id="__boim"></div>', `<div id="__boim">${app}</div>`); 
+    
     return res.send(htmlFile);
   });
 });
@@ -33,14 +34,3 @@ app.use(express.static("../../../dist"));
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-// [V] build:client -> html, js 각각 1:1 생성 확인
-// [V] html 생성, script 주입 확인
-//    -> [V] 각각의 html 안에는 기본으로 <div id="boim"></div> 존재해야 한다.
-//    -> [] 각각의 html 안에는 어떤 script가 있어야 하는가
-//    -> [] div안에 컴포넌트의 react component jsx를 주입하는 시점은 
-//          express에서 반환하기 직전이 적절한가
-
-// [V] build:server -> server.js 생성(express on)
-// [] pages 디렉토리 순회하여 모두 적용
-// [] app.get router -> 요청 url에 대한 처리
