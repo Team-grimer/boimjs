6caf0e44c758b0a84379eaffad46d97b95ac6300
eunoia2040 <eunoia2040@gmail.com>
2022-06-12 01:01:49 +0900

    refactor: Remove comment

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 const config = {
   preset: "ts-jest/presets/js-with-ts",
-  verbose: true, //실행 중에 각 개별 테스트를 보고해야 하는지 여부
+  verbose: true,
   rootDir: "test",
-  //testEnvironment: "node", //테스트에 사용할 테스트 환경 or jsdom?????
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
   },
