9e07770dd9254dc1a46221d92bddc2e8fa4d5ee3
seodongsu <xl445566@naver.com>
2022-06-13 01:42:57 +0900

    test: Add contextApi, directoryApi

--- packages/boim/jest.config.js
+++ packages/boim/jest.config.js
@@ -14,6 +14,7 @@ const config = {
   testMatch: ["**/*.test.js", "**/*.test.ts", "**/*.test.tsx"],
   coveragePathIgnorePatterns: ["/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testEnviroment: "jsdom",
 };
 
 module.exports = config;
