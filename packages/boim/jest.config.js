46cd21dcc0fe6d9f95434c772826e0b67c4bc449
seodongsu <xl445566@naver.com>
2022-06-14 00:59:10 +0900

    refactor: Change jest.config.js fix testenvironment, tsconfig module exnext

--- packages/boim/jest.config.js
+++ packages/boim/jest.config.js
@@ -14,7 +14,7 @@ const config = {
   testMatch: ["**/*.test.js", "**/*.test.ts", "**/*.test.tsx"],
   coveragePathIgnorePatterns: ["/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testEnviroment: "jsdom",
+  testEnvironment: "jsdom",
 };
 
 module.exports = config;
