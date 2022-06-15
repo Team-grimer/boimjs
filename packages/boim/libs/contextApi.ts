26694cec5a49b9bbe6cd82e1b3729a60c0eb6c36
woojeong kong <coolstevevc@gmail.com>
2022-06-15 20:25:17 +0900

    feat: Add Route Context and Change Context naming

--- packages/boim/libs/contextApi.ts
+++ packages/boim/libs/contextApi.ts
@@ -1,23 +1,47 @@
 import React, { createContext, ReactElement } from "react";
 
-type State = {
+interface route {
+  path: string | null;
+}
+
+type htmlState = {
   context: {
     main: ReactElement | null;
     srcList: Array<string> | null;
-    head: React.ReactNode | null;
   } | null;
-  setHead: (arg: React.ReactNode) => void | null;
+};
+
+type headState = {
+  setHead: (headChildren: React.ReactNode) => void | null;
+};
+
+type routeState = {
+  routeContext: {
+    path: string | null;
+  };
+  setRouteContext: (router: route) => void | null;
 };
 
 export default class Context {
-  static HtmlContext = createContext<State>({
+  static HtmlContext = createContext<htmlState>({
     context: {
       main: null,
-      head: null,
       srcList: null,
     },
+  });
+
+  static HeadContext = createContext<headState>({
     setHead: function () {},
   });
 
-  static ContextProvider = this.HtmlContext.Provider;
+  static RouterContext = createContext<routeState>({
+    routeContext: {
+      path: "",
+    },
+    setRouteContext: null,
+  });
+
+  static HtmlProvider = this.HtmlContext.Provider;
+  static HeadProvider = this.HeadContext.Provider;
+  static RouterProvider = this.RouterContext.Provider;
 }
