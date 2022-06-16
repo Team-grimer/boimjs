98544da67ccb989aa4f8bcf060e1fb0cfe513e13
seodongsu <xl445566@naver.com>
2022-06-17 00:31:30 +0900

    feat: add directoryApi(parseJsonSync), searchApi(getInjectionFile)

--- packages/boim/server/controller.ts
+++ packages/boim/server/controller.ts
@@ -9,6 +9,7 @@ import { getHTML } from "../pages/templates/htmlTemplate";
 import Directory from "../libs/directoryApi";
 import pathAlias from "../libs/pathAlias";
 import Fetch from "../libs/fetchApi";
+import Search from "../libs/searchApi";
 
 interface Client {
   default: ReactElement;
@@ -21,13 +22,19 @@ interface Data {
   renderProps: object;
 }
 
+interface Injection {
+  html: string | null;
+  css: string | null;
+  js: string | null;
+}
+
+const dir = new Directory();
+
 export default async function handleGetPage(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<Response<unknown, Record<string, unknown>> | void> {
-  console.log("LOG ---->", req.url); // 요청 URL 체크
-
   if (req.url.endsWith("/favicon.ico")) {
     res.set("Cache-Control", "public, must-revalidate, max-age=31557600");
     return res.end();
@@ -47,22 +54,26 @@ export default async function handleGetPage(
   }
 
   const url: string = req.url.endsWith("/") ? req.url : req.url + "/";
+  const manifestData: object = dir.parseJsonSync(
+    `${pathAlias.client}/dist/manifest.json`
+  );
+  const injectionFile: Injection = Search.getInjectionFile(
+    manifestData,
+    url,
+    req.url
+  );
   const htmlfilePath: string = path.join(
     pathAlias.client,
     "/dist/pages",
-    url,
-    "index.html"
+    manifestData[injectionFile.html]
   );
   const html: string = fs.readFileSync(htmlfilePath, "utf-8");
-
   const client: Client = require(`../../../../pages${url}index.js`);
-
   const type: string = client.SSG ? "SSG" : client.SSR ? "SSR" : "DEFAULT";
   const Component: ReactElement = client.default;
   const result: Data = await Fetch.getProps(type, client[type]);
-  const cssList: Array<string> = [url + "index.css"];
-  const scriptList: Array<string> = [url + "index.js"];
-
+  const cssList: Array<string> = [manifestData[injectionFile.css]];
+  const scriptList: Array<string> = [manifestData[injectionFile.js]];
   const app: string = getHTML(
     Component,
     result.renderProps,
@@ -77,7 +88,6 @@ export default async function handleGetPage(
   }
 
   if (html !== app) {
-    const dir = new Directory();
     dir.clearWriteSync(htmlfilePath);
     dir.updateWriteSync(htmlfilePath, app);
   }
