77ca91eaa8c2f4a3015476c2fc50eb4277d0e1b7
woojeong kong <coolstevevc@gmail.com>
2022-06-17 02:36:11 +0900

    feat: Add HeadContext,RouterContext and searchApi test code

--- packages/boim/test/searchApi.test.js
+++ packages/boim/test/searchApi.test.js
@@ -1,12 +1,14 @@
 import fs from "fs";
 
 import Search from "../libs/searchApi";
+import PathAlias from "../libs/pathAlias";
 
 describe("searchApi test", () => {
   test("type test", () => {
     expect(typeof Search.appPath).toBe("undefined");
     expect(typeof Search.documentPath).toBe("undefined");
-    expect(typeof Search.getComponentPath).toBe("function");
+    expect(typeof Search.getFileList).toBe("function");
+    expect(typeof Search.getBaseComponentPath).toBe("function");
     expect(typeof Search.getInjectionFile).toBe("function");
   });
 
@@ -23,5 +25,41 @@ describe("searchApi test", () => {
     expect(injectionFile.html).toBe("./post/index.html");
     expect(injectionFile.css).toBe("/post/index.css");
     expect(injectionFile.js).toBe("/post/index.js");
+  });
+
+  test("getFileList static metho test", () => {
+    const tempFileList = fs.readdirSync(`${PathAlias.client}/pages`);
+    const fileList = Search.getFileList(`${PathAlias.client}/pages`);
+
+    expect(tempFileList.length).toBe(fileList.length);
+  });
+
+  test("getBaseComponentPath static metho test", () => {
+    const mockFileListWithoutDocumentFile = ["_app.js"];
+    const mockFileListWithoutAppFile = ["_document.js"];
+    const mockFileListWithoutBothFile = ["index.js"];
+
+    const resultWithoutDocumentFile = {
+      _app: `${PathAlias.client}/pages/_app.js`,
+      _document: `${PathAlias.root}/pages/_document.tsx`,
+    };
+    const resultWithoutAppFile = {
+      _app: `${PathAlias.root}/pages/_app.tsx`,
+      _document: `${PathAlias.client}/pages/_document.js`,
+    };
+    const resultWithoutBothFile = {
+      _app: `${PathAlias.root}/pages/_app.tsx`,
+      _document: `${PathAlias.root}/pages/_document.tsx`,
+    };
+
+    expect(
+      Search.getBaseComponentPath(mockFileListWithoutDocumentFile)
+    ).toStrictEqual(resultWithoutDocumentFile);
+    expect(
+      Search.getBaseComponentPath(mockFileListWithoutAppFile)
+    ).toStrictEqual(resultWithoutAppFile);
+    expect(
+      Search.getBaseComponentPath(mockFileListWithoutBothFile)
+    ).toStrictEqual(resultWithoutBothFile);
   });
 });
