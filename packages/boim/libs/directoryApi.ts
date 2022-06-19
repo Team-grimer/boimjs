1c363f39daf80037ad57815cc8b2a23747b69a4d
eunoia2040 <eunoia2040@gmail.com>
2022-06-19 20:17:59 +0900

    feat: Add getDynamicPaths, writeDynamicHydrateComponent method in directoryApi

--- packages/boim/libs/directoryApi.ts
+++ packages/boim/libs/directoryApi.ts
@@ -1,8 +1,12 @@
 import fs from "fs";
 import path from "path";
 
+import "../config/fetchPolyfill";
 import pathAlias from "./pathAlias";
 
+interface pageProps {
+  props: object;
+}
 export default class Directory {
   filePaths: { [key: string]: string };
   cssFiles: { [key: string]: string };
@@ -20,10 +24,41 @@ export default class Directory {
     return this.cssFiles;
   }
 
+  getDynamicPaths(startDirectoryPath: string): { [key: string]: string } {
+    const dynamicPaths: { [key: string]: string } = {};
+    const recursivelySearchDynamicPath = (directoryPath: string): void => {
+      fs.readdirSync(directoryPath).forEach((file: string) => {
+        const Absolute: string = path.join(directoryPath, file);
+
+        if (
+          fs.statSync(Absolute).isDirectory() &&
+          !(file.startsWith("[") && file.endsWith("]"))
+        ) {
+          recursivelySearchDynamicPath(Absolute);
+          return;
+        }
+
+        if (file.startsWith("[") && file.endsWith("]")) {
+          const fileKey: string = Absolute.replace(startDirectoryPath, "");
+          const fileName: string = fs.readdirSync(Absolute)[0];
+
+          dynamicPaths[fileKey] = `${Absolute}/${fileName}`;
+        }
+      });
+    };
+    recursivelySearchDynamicPath(startDirectoryPath);
+
+    return dynamicPaths;
+  }
+
   searchDirectory(startDirectoryPath: string): void {
     const recursivelySearchDirectory = (directoryPath: string): void => {
       fs.readdirSync(directoryPath).forEach((file: string) => {
         const Absolute: string = path.join(directoryPath, file);
+
+        if (file.startsWith("[") && file.endsWith("]")) {
+          return;
+        }
 
         if (fs.statSync(Absolute).isDirectory()) {
           recursivelySearchDirectory(Absolute);
@@ -58,41 +93,34 @@ export default class Directory {
       recursive: true,
     });
 
-    function isFile(dir: string): boolean {
+    const isFile = (dir: string): boolean => {
       return dir.lastIndexOf("/") === 0;
-    }
+    };
 
-    function isRoot(dir: string): boolean {
+    const isRoot = (dir: string): boolean => {
       return dir === "/index";
-    }
+    };
 
     for (const dir of Object.keys(entries)) {
       const componentsPath = `${pathAlias.client}/pages`;
       const outPath = `${pathAlias.root}/client/hydratedComponents`;
-
       const content = `import React from "react";
-import ReactDOM from "react-dom";
-import * as App from "${componentsPath + dir}.js";
-import Fetch from "${pathAlias.root}/libs/fetchApi";
-import Route from "${pathAlias.root}/pages/Route";
+        import ReactDOM from "react-dom";
+        import * as App from "${componentsPath + dir}.js";
+        import Fetch from "${pathAlias.root}/libs/fetchApi";
+        import Route from "${pathAlias.root}/pages/Route";
 
-const app = {};
+        const app = Object.assign({}, App);
+        const Component = app["default"];
+        const type = app["SSG"] ? "SSG" : app["SSR"] ? "SSR" : "DEFAULT";
+        async function hydrate() {
+          const result = await Fetch.getProps(type, app[type]);
+          const container = document.getElementById("__boim");
+          ReactDOM.hydrate(<Route initialInfo={{ result, Component }} />, container);
+        }
+        hydrate();
+      `;
 
-Object.entries(App).forEach(([key, value]) => {
-  app[key] = value;
-});
-
-const Component = app["default"];
-const type = app["SSG"] ? "SSG" : app["SSR"] ? "SSR" : "DEFAULT";
-
-async function hydrate() {
-  const result = await Fetch.getProps(type, app[type]);
-  const container = document.getElementById("__boim");
-
-  ReactDOM.hydrate(<Route initialInfo={{ result, Component }} />, container);
-}
-hydrate();
-`;
       try {
         !isFile(dir) &&
           !isRoot(dir) &&
@@ -102,6 +130,75 @@ hydrate();
         fs.writeFileSync(path.join(outPath, `${dir}.js`), content);
       } catch (e) {
         console.log("Cannot create folder and file ", e);
+      }
+    }
+  }
+
+  writeDynamicHydrateComponent(files: { [key: string]: string }): void {
+    fs.mkdirSync(`${pathAlias.root}/client/dynamicComponents`, {
+      recursive: true,
+    });
+
+    for (const directoryPath of Object.keys(files)) {
+      const dynamicComponent = require(`../../../../pages${directoryPath}/index.js`);
+      const app = Object.assign({}, dynamicComponent);
+
+      const createDynamicFiles = async (): Promise<void> => {
+        const { paths } = await app.getStaticPaths();
+
+        paths.forEach(async (param) => {
+          const pageProps: pageProps = await app.SSG(param);
+          const props = JSON.stringify({
+            renderType: "StaticSiteGeneration",
+            renderProps: { props: pageProps.props },
+          });
+
+          const content = `
+            import React from "react";
+            import ReactDOM from "react-dom";
+            import Component from "${pathAlias.client}/pages${directoryPath}/index.js";
+            import Route from "${pathAlias.root}/pages/Route";
+
+            const result = ${props}
+            const container = document.getElementById("__boim");
+            ReactDOM.hydrate(<Route initialInfo={{ result, Component }} />, container);
+          `;
+
+          const key: string = directoryPath
+            .split("/")
+            .pop()
+            .replace("[", "")
+            .replace("]", "");
+          const paramKey: string = param.params[key];
+          const outputKey: string = String(directoryPath).replace(
+            `[${key}]`,
+            paramKey
+          );
+          const outDir = `${pathAlias.root}/client/dynamicComponents`;
+
+          try {
+            fs.mkdirSync(
+              path.join(outDir, String(directoryPath).replace(`/[${key}]`, "")),
+              {
+                recursive: true,
+              }
+            );
+            fs.writeFileSync(path.join(outDir, `${outputKey}.js`), content);
+            fs.writeFileSync(
+              path.join(outDir, `${outputKey}.json`),
+              JSON.stringify({
+                ...JSON.parse(props),
+                routeKey: key,
+              })
+            );
+          } catch (e) {
+            console.log("Cannot create folder and file ", e);
+          }
+        });
+      };
+
+      if (dynamicComponent.hasOwnProperty("getStaticPaths")) {
+        createDynamicFiles();
       }
     }
   }
