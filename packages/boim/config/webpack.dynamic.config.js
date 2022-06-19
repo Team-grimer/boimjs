21914cac528b45d0701959034d89bc66eb942ec5
eunoia2040 <eunoia2040@gmail.com>
2022-06-19 20:15:53 +0900

    feat: Add wepack config for dynamic route

---
+++ packages/boim/config/webpack.dynamic.config.js
@@ -0,0 +1,156 @@
+const path = require("path");
+
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+const root = path.resolve("./");
+const client = path.resolve(root, "../../../");
+
+const Directory = require(`${client}/dist/lib/directoryApi`).default;
+const dir = new Directory();
+
+dir.searchDirectory(`${root}/client/dynamicComponents`);
+const hydratedDynamicComponentEntries = dir.getFilePaths();
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  entry: hydratedDynamicComponentEntries,
+  output: {
+    path: `${client}/dist/pages`,
+    filename: "[name][chunkhash].js",
+    library: "build-page",
+    libraryTarget: "umd",
+    globalObject: "this",
+    assetModuleFilename: "../public/[contenthash][ext]",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    alias: {
+      react: `${client}/node_modules/react`,
+      "react-dom": `${client}/node_modules/react-dom`,
+    },
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "ts-loader" },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(less|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              emit: true,
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.module.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              emit: true,
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              emit: true,
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.(txt)$/i,
+        type: "asset/source",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: ".[name].html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name][contenthash].css",
+    }),
+    new WebpackManifestPlugin({
+      fileName: "../dynamicManifest.json",
+    }),
+  ],
+};
