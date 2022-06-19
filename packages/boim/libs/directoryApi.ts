52e093f79a6c5024af8ade9c3928b6b926ea2d08
seodongsu <xl445566@naver.com>
2022-06-19 23:29:53 +0900

    fix: Complete testcode error,  controller cashe logic, directoryApi logic

--- packages/boim/libs/directoryApi.ts
+++ packages/boim/libs/directoryApi.ts
@@ -197,7 +197,7 @@ export default class Directory {
         });
       };
 
-      if (dynamicComponent.hasOwnProperty("getStaticPaths")) {
+      if (dynamicComponent.hasOwnProperty("PATHS")) {
         createDynamicFiles();
       }
     }
