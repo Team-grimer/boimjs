1f469dd39e42a06b6b21e175530dd5de7c765c18
woojeong kong <coolstevevc@gmail.com>
2022-06-19 18:08:47 +0900

    feat: Add head rendering logic about client side rendering

--- packages/boim/pages/Route.tsx
+++ packages/boim/pages/Route.tsx
@@ -1,27 +1,30 @@
 import React, { useState, useEffect, ReactElement } from "react";
 
 import qs from "querystringify";
+import reactElementToJSXString from "react-element-to-jsx-string";
 
 import Context from "../libs/contextApi";
 import Fetch from "../libs/fetchApi";
 import History from "../libs/historyApi";
 
-interface RouteProps {
-  initialInfo: {
-    result: {
-      [key: string]: any;
-    };
-    Component: React.FC;
+interface Props {
+  _App: React.ElementType;
+  result: {
+    [key: string]: any;
   };
+  Component: ReactElement;
+}
+
+interface RouteProps {
+  initialInfo: Props;
 }
 
 interface PageProps {
-  componentInfo: {
-    result: {
-      [key: string]: any;
-    };
-    Component: React.FC;
-  };
+  componentInfo: Props;
+}
+
+interface PageHeadProps {
+  headList: Array<ReactElement>;
 }
 
 interface RouteState {
@@ -36,7 +39,7 @@ interface RouteState {
 }
 
 interface RenderInfo {
-  context: {
+  routerContext: {
     push: (pathName: string, param: { [key: string]: any }) => void;
     path: string;
     param: {
@@ -46,26 +49,107 @@ interface RenderInfo {
       [key: string]: string;
     };
   };
-  renderOption: {
-    result: {
-      [key: string]: any;
-    };
-    Component: React.FC;
+  headContext: {
+    cssList: Array<string>;
+    headInstance: Set<unknown>;
+    setHead: (headList: any) => void;
   };
+  renderOption: Props;
 }
 
-const { RouterProvider } = Context;
-const history = History.history;
+const { RouterProvider, HeadProvider } = Context;
+const { history } = History;
+
+const defaultHeadTag = `<head><meta charSet="utf-8"></meta>
+<meta name="viewport" content="width=device-width,initial-scale=1"></meta>
+<title>Boim js</title></head>`;
+const headTargetList: Array<string> = [
+  "meta[charset]",
+  "meta[name=\"viewport\"]",
+  "title",
+];
 
 const MemoedPage: React.FC<PageProps> = React.memo(Page);
 
 function Page({ componentInfo }: PageProps): ReactElement {
   return (
-    <componentInfo.Component {...componentInfo.result.renderProps.props} />
+    <componentInfo._App
+      Component={componentInfo.Component}
+      pageProps={componentInfo.result.renderProps}
+    />
   );
 }
 
+function PageHead({ headList }: PageHeadProps): ReactElement {
+  if (headList.length === 0) {
+    const headDocument = document.querySelector("head");
+
+    const DomParser = new DOMParser();
+
+    const defaultHeadNode = DomParser.parseFromString(
+      defaultHeadTag,
+      "text/html"
+    ).querySelector("head");
+
+    while (headDocument.firstChild) {
+      headDocument.removeChild(headDocument.lastChild);
+    }
+
+    Object.entries(defaultHeadNode.children).forEach(([_, value]) => {
+      headDocument.appendChild(value);
+    });
+
+    return null;
+  }
+
+  const headElementList = headList.flat();
+
+  const headDocument = document.querySelector("head");
+  const DomParser = new DOMParser();
+
+  const defaultHeadNode = DomParser.parseFromString(
+    defaultHeadTag,
+    "text/html"
+  ).querySelector("head");
+
+  const headTagString = headElementList
+    .map((value) => {
+      return reactElementToJSXString(value);
+    })
+    .join("");
+
+  const customHeadDocument = DomParser.parseFromString(
+    headTagString,
+    "text/html"
+  );
+  const customHeadNode = customHeadDocument.querySelector("head");
+
+  while (headDocument.firstChild) {
+    headDocument.removeChild(headDocument.lastChild);
+  }
+
+  Object.entries(defaultHeadNode.children).forEach(([_, value]) => {
+    headDocument.appendChild(value);
+  });
+
+  headTargetList.forEach((value) => {
+    if (customHeadNode.querySelector(value)) {
+      headDocument.querySelector(value).remove();
+    }
+  });
+
+  Object.entries(customHeadNode.children).forEach(([_, value]) => {
+    headDocument.appendChild(value);
+  });
+
+  return null;
+}
+
+const MemoedPageHead: React.FC<PageHeadProps> = React.memo(PageHead);
+
 export default function Route({ initialInfo }: RouteProps): ReactElement {
+  const [pageHeadList, setPageHeadList] = useState([]);
+
   const [routeStatus, setRouteStatus] = useState<RouteState>({
     isSSR: true,
     path: "",
@@ -74,13 +158,20 @@ export default function Route({ initialInfo }: RouteProps): ReactElement {
   });
 
   const [renderInfo, setRenderInfo] = useState<RenderInfo>({
-    context: {
+    routerContext: {
       push: (pathName, state) => {
         history.push(pathName, state);
       },
       path: "",
       param: {},
       query: {},
+    },
+    headContext: {
+      cssList: null,
+      headInstance: new Set(),
+      setHead: (headList) => {
+        setPageHeadList(headList);
+      },
     },
     renderOption: {
       ...initialInfo,
@@ -95,6 +186,7 @@ export default function Route({ initialInfo }: RouteProps): ReactElement {
         param: location.state,
         query: qs.parse(location.search),
       });
+      // setPageHeadList([]);
     });
 
     return () => {
@@ -107,29 +199,35 @@ export default function Route({ initialInfo }: RouteProps): ReactElement {
       return;
     }
 
-    const App = require(`../../../../pages${routeStatus.path}`);
+    const client = require(`../../../../pages${routeStatus.path}`);
     const app = {};
 
-    Object.entries(App).forEach(([key, value]) => {
+    Object.entries(client).forEach(([key, value]) => {
       app[key] = value;
     });
 
-    const Component: React.FC = app["default"];
+    const Component: ReactElement = app["default"];
     const type: string = app["SSG"] ? "SSG" : app["SSR"] ? "SSR" : "DEFAULT";
 
+    const _App: React.ElementType = initialInfo._App;
+
     async function getComponentProps() {
-      const componentProps = await Fetch.getProps(type, app[type]);
+      const result = await Fetch.getProps(type, app[type]);
 
       setRenderInfo({
-        context: {
-          ...renderInfo.context,
+        routerContext: {
+          ...renderInfo.routerContext,
           path: routeStatus.path,
           param: routeStatus.param,
           query: routeStatus.query,
         },
+        headContext: {
+          ...renderInfo.headContext,
+        },
         renderOption: {
+          _App,
+          result,
           Component,
-          result: componentProps,
         },
       });
     }
@@ -137,16 +235,14 @@ export default function Route({ initialInfo }: RouteProps): ReactElement {
     getComponentProps();
   }, [routeStatus]);
 
-  const headContextValue = {
-    cssList: null,
-    setHead: (headChildren) => {
-      return null;
-    },
-  };
-
   return (
-    <RouterProvider value={renderInfo.context}>
-      <MemoedPage componentInfo={renderInfo.renderOption} />
-    </RouterProvider>
+    <>
+      {!routeStatus.isSSR && <MemoedPageHead headList={pageHeadList} />}
+      <RouterProvider value={renderInfo.routerContext}>
+        <HeadProvider value={renderInfo.headContext}>
+          <MemoedPage componentInfo={renderInfo.renderOption} />
+        </HeadProvider>
+      </RouterProvider>
+    </>
   );
 }
