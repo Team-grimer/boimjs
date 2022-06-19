d34fb5d53afcff34308096604a8ed80f5a720818
woojeong kong <coolstevevc@gmail.com>
2022-06-19 18:05:41 +0900

    feat: Update head rendering logic

--- packages/boim/pages/templates/htmlTemplate.tsx
+++ packages/boim/pages/templates/htmlTemplate.tsx
@@ -3,10 +3,9 @@ import React, { ReactElement } from "react";
 import _Document from "document";
 import _App from "app";
 import ReactDOMServer from "react-dom/server";
+import { parse } from "node-html-parser";
 
 import Context from "../../libs/contextApi";
-
-const { HtmlProvider, HeadProvider } = Context;
 
 interface HTMLProps {
   main: ReactElement;
@@ -18,24 +17,21 @@ interface HEADProps {
   cssList?: Array<string> | null;
 }
 
-function DefaultHead(): ReactElement {
-  return (
-    <>
-      <meta charSet="utf-8"></meta>
-      <meta name="viewport" content="width=device-width,initial-scale=1"></meta>
-      <title>Boim js</title>
-    </>
-  );
-}
+const { HtmlProvider, HeadProvider } = Context;
+
+const headTargetList: Array<string> = [
+  "meta[charset]",
+  'meta[name="viewport"]',
+  "title",
+];
+const defaultHeadTag = `<head><meta charSet="utf-8"></meta>
+<meta name="viewport" content="width=device-width,initial-scale=1"></meta>
+<title>Boim js</title></head>`;
 
 function CustomHead({ headList, cssList }: HEADProps): ReactElement {
   return (
     <head>
-      {headList.length ? (
-        <>{headList.map((value) => value)}</>
-      ) : (
-        <DefaultHead />
-      )}
+      <>{headList.map((value) => value)}</>
 
       {cssList.length ? (
         <>
@@ -54,6 +50,21 @@ function renderPageTree(
   pageProps: object
 ): ReactElement {
   return <App Component={Component} pageProps={pageProps} />;
+}
+
+function setHead(defaultHeadTag, customHeadTagString, targetList) {
+  const originalDocument = parse(defaultHeadTag);
+  const customHeadDocument = parse(customHeadTagString);
+
+  targetList.forEach((value) => {
+    if (customHeadDocument.querySelector(value)) {
+      originalDocument.querySelector(value).remove();
+    }
+  });
+
+  originalDocument.querySelector("head").appendChild(customHeadDocument);
+
+  return originalDocument.toString();
 }
 
 export function getHTML(
@@ -75,6 +86,7 @@ export function getHTML(
 
   const headContextValue = {
     cssList,
+    headInstance: new Set(),
     setHead: (headChildren) => {
       if (headChildren) headComponentList.push(headChildren);
     },
@@ -89,12 +101,14 @@ export function getHTML(
   );
 
   let html: string = ReactDOMServer.renderToString(document);
-  const head: string = ReactDOMServer.renderToString(
+  const customHeadTag: string = ReactDOMServer.renderToString(
     <CustomHead
       headList={headComponentList}
       cssList={headContextValue.cssList}
     />
   );
+
+  const head: string = setHead(defaultHeadTag, customHeadTag, headTargetList);
 
   html = html.replace("<head></head>", head);
 
