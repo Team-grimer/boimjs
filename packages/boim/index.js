e1b01cdcedb227eabaa4d472c0ba1bfc15e71e08
eunoia2040 <eunoia2040@gmail.com>
2022-06-25 02:02:32 +0900

    refactor: Remove index.js

--- packages/boim/index.js
+++
@@ -1,5 +0,0 @@
-import { Html, Head, Main, Script } from "./pages/_document";
-import Link from "./pages/Link";
-import { useRouter } from "./pages/Route";
-
-export { Html, Head, Main, Script, Link, useRouter };
