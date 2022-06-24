485736eeacddb91d37d24edfd06743cd47558e01
eunoia2040 <eunoia2040@gmail.com>
2022-06-25 01:10:00 +0900

    refactor: Add root file for module export

---
+++ packages/boim/index.js
@@ -0,0 +1,5 @@
+import { Html, Head, Main, Script } from "./pages/_document";
+import Link from "./pages/Link";
+import { useRouter } from "./pages/Route";
+
+export { Html, Head, Main, Script, Link, useRouter };
